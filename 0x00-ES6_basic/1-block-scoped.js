46dfd8e3c959d55ac4a44c370e5c466e362893cc
ismael <spacingismael@gmail.com>
2024-09-30 15:49:51 +0100

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-// export default function taskBlock(trueOrFalse) {
-//   let task = false;
-//   let task2 = true;
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
 
-//   if (trueOrFalse) {
-//     task2 = true;
-//     task = false;
-//   }
+  if (trueOrFalse) {
+    task2 = true;
+    task = false;
+  }
 
-//   return [task, task2];
-// }
+  return [task, task2];
+}
