54de46f6e7a2190a026c525857f28cd4bf7f71a9
ismael <spacingismael@gmail.com>
2024-09-30 15:48:34 +0100

    es6 directory

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// export default function taskBlock(trueOrFalse) {
+//   let task = false;
+//   let task2 = true;
+
+//   if (trueOrFalse) {
+//     task2 = true;
+//     task = false;
+//   }
+
+//   return [task, task2];
+// }
