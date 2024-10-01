b1291b79bd17e102ee577ddcfa409e5bfd82e8c3
ismael <spacingismael@gmail.com>
2024-10-01 23:42:12 +0100

    fixing task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,10 @@
-#!/usr/bin/node
-
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+  return Promise
+    .all([uploadPhoto(), createUser()])
     .then((vals) => {
-      console.log(`${vals[0].body} ${vals[1].firstName} ${vals[1].lastName}`);
+      console.log(`${vals[0]?.body} ${vals[1]?.firstName} ${vals[1]?.lastName}`);
     })
     .catch(() => console.log('Signup system offline '));
 }
