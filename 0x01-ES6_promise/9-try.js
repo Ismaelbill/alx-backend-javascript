dd8feab98e9cb8b5fe69ab7b658a0b657c61a0df
ismael <spacingismael@gmail.com>
2024-10-02 03:28:10 +0100

    task 8, 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+export default function guardrail(mathFunction) {
+  const queue = [];
+  let val;
+  try {
+    val = mathFunction();
+    queue.push(val);
+  } catch (msg) {
+    queue.push(String(msg));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
