eaf6f2ff9512b49b5c6d18c5e9811c4e314c2d93
ismael <spacingismael@gmail.com>
2024-10-02 03:53:56 +0100

    task 100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const photObj = await uploadPhoto();
+    const userObj = await createUser();
+    return { photObj, userObj };
+  } catch (err) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
