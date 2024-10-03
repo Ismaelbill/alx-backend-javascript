a38d0f0d08215a33c5659127e1fceb9542ad9526
ismael <spacingismael@gmail.com>
2024-10-04 00:25:36 +0100

    task 7, 8

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,1 +1,28 @@
 #!/usr/bin/node
+
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    this._name = val;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    this._code = val;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,32 @@
 #!/usr/bin/node
+
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._floors;
+  }
+
+  set size(val) {
+    this._size = val;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(val) {
+    this._location = val;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
