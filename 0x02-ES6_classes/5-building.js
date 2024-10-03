3a32a54985cd28d7eaa11c5be5f2f1a10ebb6f48
ismael <spacingismael@gmail.com>
2024-10-03 23:17:51 +0100

    task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+}
