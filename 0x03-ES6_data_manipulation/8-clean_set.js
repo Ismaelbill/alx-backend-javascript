c267984e97bed3520730882043659e860d97e658
ismael <spacingismael@gmail.com>
2024-10-07 23:25:12 +0100

    task 8#1

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,10 +3,10 @@
 function cleanSet(set, startString) {
   let str = '';
 
-  //   if (startString.length === 0) return str;
+  if (startString.length === 0) return str;
 
   for (const word of Array(...set)) {
-    if (word.startsWith(startString)) {
+    if (typeof word === 'string' && word.startsWith(startString)) {
       str = str.concat(word.slice(startString.length), '-');
     }
   }
