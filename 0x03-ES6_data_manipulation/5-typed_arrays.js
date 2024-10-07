6ff1616da3cadd127ff00a5b03438c9661ca2cfc
ismael <spacingismael@gmail.com>
2024-10-07 17:18:55 +0100

    task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  try {
+    view.setUint8(position, value);
+  } catch (s) {
+    throw Error('Position outside range');
+  }
+  return view;
+}
+export default createInt8TypedArray;
