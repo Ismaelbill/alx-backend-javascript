8f9f8617887a04d9e722639202f3b7337185df31
ismael <spacingismael@gmail.com>
2024-10-08 00:11:20 +0100

    fixing task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,9 @@
 function cleanSet(set, startString) {
   let str = '';
 
-  if (startString.length === 0) return str;
+  if (startString.length === 0 || String(set).slice(8, 11) !== 'Set') {
+    return str;
+  }
 
   for (const word of Array(...set)) {
     if (typeof word === 'string' && word.startsWith(startString)) {
