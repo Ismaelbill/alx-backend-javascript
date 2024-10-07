9a0db4275e3eecce932ddd573aef8d37e53d6a26
ismael <spacingismael@gmail.com>
2024-10-08 00:00:01 +0100

    task 100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+export const weakMap = new WeakMap();
+
+export function queryAPI(obj) {
+  if (!weakMap.get(obj)) weakMap.set(obj, 0);
+  weakMap.set(obj, weakMap.get(obj) + 1);
+  if (weakMap.get(obj) >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
