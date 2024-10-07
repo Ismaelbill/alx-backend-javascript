dbe78bd974b7e407c0b9ce16d6e7b02ffbfd03ec
ismael <spacingismael@gmail.com>
2024-10-07 22:27:12 +0100

    task 7, 8

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+function hasValuesFromArray(set, arr) {
+  for (const j of arr) {
+    if (!set.has(j)) {
+      return false;
+    }
+  }
+  return true;
+}
+export default hasValuesFromArray;
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+function cleanSet(set, startString) {
+  let str = '';
+
+  if (startString.length === 0) return str;
+
+  for (const word of Array(...set)) {
+    if (word.startsWith(startString)) {
+      str = str.concat(word.slice(startString.length), '-');
+    }
+  }
+  return str.slice(0, str.length - 1);
+}
+export default cleanSet;
