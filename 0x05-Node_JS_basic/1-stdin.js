37fe7f19dea1ca12839f06754bb21e1d677b23fb
ismael <spacingismael@gmail.com>
2024-11-25 22:42:18 +0100

    fixing task 1#1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 
-console.log('Welcome to Holberton School, what is your name?');
+process.stdin.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.toString().trim()}`);
+  process.stdin.write(`Your name is: ${data.toString().trim()}\n`);
   process.exit();
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdin.write('This important software is now closing\n');
+  process.exit()
 });
