4c5a17708f395ba01b2377259ad0a9ff6b0ed21f
ismael <spacingismael@gmail.com>
2024-11-26 06:09:12 +0100

    task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+const fs = require('fs');
+
+function countStudents(path) {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    const arr = data.split('\n').slice(1);
+    const cs = [];
+    const swe = [];
+    for (const line of arr) {
+      const fl = line.split(',').slice(-1)[0];
+      const name = line.split(',').slice(0)[0];
+      if (fl && fl === 'CS') {
+        cs.push(name);
+      } else if (fl && fl === 'SWE') {
+        swe.push(name);
+      }
+    }
+    console.log(`Number of students: ${cs.length + swe.length}`);
+    console.log(`Number of students in CS: ${cs.length}. List:`, cs.join(', '));
+    console.log(`Number of students in SWE: ${swe.length}. List:`, swe.join(', '));
+  });
+}
+
+module.exports = countStudents;
